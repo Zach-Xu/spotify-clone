c79640c09b1cb884a44fb72e88b352c1e125de56
Zach Xu <zach.popping@gmail.com>
2023-02-15 01:19:54 -0500

    project setup

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <h1>This is a Spotify Clone Build</h1>
+
+
+    </div>
+  )
+}
+
+export default Home
